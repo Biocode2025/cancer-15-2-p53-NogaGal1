ce23478fac669dfb713eeaad4dc373c85532ea2f
NogaGal1 <nogagal311@gmail.com>
2026-01-19 11:26:25 +0200

    Add Mutate_DNA function

---
+++ scripts/cancercode.py
@@ -0,0 +1,23 @@
+import random
+RNA_codon_table = {}
+
+#function that replaces a nucleotid
+def Mutate_DNA(seq):
+  Mutated_seq = ""
+  nuc_list = ["A", "T", "G", "C"]
+  seq = seq.upper()
+  
+  #replace a nucleotid in the given sequence with a random nucleotid
+  rand_index = random.randrange(0, len(seq))
+  rand_nuc = seq[rand_index]
+  nuc_list.remove(rand_nuc)
+  rand_base = random.randrange(0, len(nuc_list))
+  new_nuc = nuc_list[rand_base]
+  
+  #create & combine the parts
+  fst_part = seq[0 : rand_index]
+  snd_part = seq[rand_index + 1 :]
+  Mutated_seq = fst_part + new_nuc + snd_part
+  
+  return Mutated_seq
+  
